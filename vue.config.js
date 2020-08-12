f15372c81770929730c501a9d69c79363eb26acc
arts1986 <arts1986@126.com>
2020-08-12 23:56:40 +0800

    alter

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const posixJoin = _path => path.posix.join('/', _path)
 module.exports = {
   // 修改 src 为 examples
   pages: {
@@ -7,10 +9,27 @@ module.exports = {
       filename: 'index.html'
     }
   },
+  productionSourceMap: false,
   // 强制内联CSS
-  css: { extract: false },
+  css: {
+    extract: {
+      filename: posixJoin(`css/jeui.css`),
+      chunkFilename: posixJoin(`css/jeui.css`)
+    },
+    sourceMap: false,
+    modules: false
+  },
   // 扩展 webpack 配置，使 packages 加入编译
   chainWebpack: config => {
 
+  },
+  // 第三方插件的选项
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.resolve(__dirname, './packages/style/variable.less')  // 引入全局样式变量
+      ]
+    }
   }
 }
